9537e903d9d6c1c4039fc12093051c37d3d2c8a6
Vera Ryabchenko <vera.ivanovna.tgl@gmail.com>
2022-09-02 14:16:31 +0400

    changed files

--- js/addToCart.js
+++ js/addToCart.js
@@ -1,11 +1,12 @@
 const cartInput = document.querySelector('.cartInput')
 const cartInputMobile = document.querySelector('.cartInputMobile')
+const removeItems = document.querySelectorAll('remove')
+
 let number = 1;
 
 document.addEventListener('click', event => {
-    if (window.screen.width > 768 && event.target.classList.contains('product_item_btn')) 
+    if (window.screen.width > 768 && event.target.classList.contains('product_item_btn')) {
         cartInput.innerHTML = '(' + number++ + ')';
-        //alert('Товар успешно добавлен')
     } else if (window.screen.width <= 768 && event.target.classList.contains('product_item_btn_mobile')) {
         cartInputMobile.innerHTML = '(' + number++ + ')';
         //alert('Товар успешно добавлен')
@@ -15,5 +16,7 @@ document.addEventListener('click', event => {
     } else if (window.screen.width <= 768 && event.target.classList.contains('clear')) {
         number = 1
         cartInputMobile.innerHTML = '';
-}
+    }
 })
+
+//${item.amount}
