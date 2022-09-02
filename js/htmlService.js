859370eb12d087a3d9525ecbb757b8d9d319b299
Vera Ryabchenko <93258582+Vera-Ryabchenko@users.noreply.github.com>
2022-09-02 15:10:20 +0400

    Update htmlService.js

--- js/htmlService.js
+++ js/htmlService.js
@@ -57,7 +57,7 @@ class HTMLService {
             <hr />
             <div class="info">
                 <span>Общая цена: <strong>$${totalPrice.toFixed(2)}</strong></span>
-                <button class="clear" data-type="clear">Empty the trash</button>
+                <button class="clear" data-type="clear">Clear the shopping cart</button>
             </div>
         `
     }
