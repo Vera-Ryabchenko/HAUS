b6b8cdf1ce179ee5096ef152572d79c81578cc46
Vera Ryabchenko <93258582+Vera-Ryabchenko@users.noreply.github.com>
2022-09-02 12:13:35 +0400

    Update addToCart.js

--- js/addToCart.js
+++ js/addToCart.js
@@ -3,13 +3,11 @@ const cartInputMobile = document.querySelector('.cartInputMobile')
 let number = 1;
 
 document.addEventListener('click', event => {
-    if (window.screen.width > 768 && event.target.classList.contains('product_item_btn')) {
-        let number1 = number++;
-        cartInput.innerHTML = '(' + number1 + ')';
+    if (window.screen.width > 768 && event.target.classList.contains('product_item_btn')) 
+        cartInput.innerHTML = '(' + number++ + ')';
         //alert('Товар успешно добавлен')
     } else if (window.screen.width <= 768 && event.target.classList.contains('product_item_btn_mobile')) {
-        let number2 = number++;
-        cartInputMobile.innerHTML = '(' + number2 + ')';
+        cartInputMobile.innerHTML = '(' + number++ + ')';
         //alert('Товар успешно добавлен')
     } else if (window.screen.width > 768 && event.target.classList.contains('clear')) {
             number = 1
